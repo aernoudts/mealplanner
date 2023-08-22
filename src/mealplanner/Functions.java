da65065a457df5b7a86930fe2d036d64a7fa7522
aernoudts <henriaernoudts@gmail.com>
2023-08-22 15:42:43 -0300

    stage 5

--- src/mealplanner/Functions.java
+++ src/mealplanner/Functions.java
@@ -5,11 +5,15 @@ import java.util.*;
 
 public class Functions {
 
-    public static void add(List<List> items, HashMap<String, String[]> ingList, int mealId, int ingId) throws SQLException {
+    public static Connection getConnection() throws SQLException {
         String DB_URL = "jdbc:postgresql:meals_db";
         String USER = "postgres";
         String PASS = "1111";
-        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+
+    public static void add(List<List> items, HashMap<String, String[]> ingList, int mealId, int ingId) throws SQLException {
+        Connection connection = getConnection();
         connection.setAutoCommit(true);
         Statement statement = connection.createStatement();
         Scanner scanner = new Scanner(System.in);
@@ -39,7 +43,6 @@ public class Functions {
                     break;
                 }
             }
-
             System.out.println("Input the ingredients: ");
             while (true) {
                 mealIngredients = scanner.nextLine();
@@ -76,11 +79,9 @@ public class Functions {
             break;
         }
     }
+
     public static void show(String category) throws SQLException {
-        String DB_URL = "jdbc:postgresql:meals_db";
-        String USER = "postgres";
-        String PASS = "1111";
-        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Connection connection = getConnection();
         connection.setAutoCommit(true);
         PreparedStatement statementCategory = connection.prepareStatement("SELECT * FROM meals WHERE category = '" + category + "'");
         ResultSet rs = statementCategory.executeQuery();
@@ -107,6 +108,7 @@ public class Functions {
         statementCategory.close();
         connection.close();
     }
+
     public static String mealCategory() {
         while (true) {
             Scanner scanner = new Scanner(System.in);
@@ -118,6 +120,140 @@ public class Functions {
             } else {
                 return category;
             }
+        }
+    }
+
+    public static void plan() throws SQLException {
+        Connection connection = getConnection();
+        connection.setAutoCommit(true);
+        Scanner scanner = new Scanner(System.in);
+        String[] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        for (String weekday : weekdays) {
+            System.out.println(weekday);
+            listBreakfastMeals();
+            System.out.println("Choose the breakfast for " + weekday + " from the list above:");
+            String breakfast = scanner.nextLine();
+            while (getMealID(breakfast) == -1) {
+                System.out.println("This meal doesn’t exist. Choose a meal from the list above.");
+                breakfast = scanner.nextLine();
+            }
+            insertMealPlan(getMealID(breakfast), breakfast, "breakfast");
+
+            listLunchMeals();
+            System.out.println("Choose the lunch for " + weekday + " from the list above:");
+            String lunch = scanner.nextLine();
+            while (getMealID(lunch) == -1) {
+                System.out.println("This meal doesn’t exist. Choose a meal from the list above.");
+                lunch = scanner.nextLine();
+            }
+            insertMealPlan(getMealID(lunch), lunch, "lunch");
+
+            listDinnerMeals();
+            System.out.println("Choose the dinner for " + weekday + " from the list above:");
+            String dinner = scanner.nextLine();
+            while (getMealID(dinner) == -1) {
+                System.out.println("This meal doesn’t exist. Choose a meal from the list above.");
+                dinner = scanner.nextLine();
+            }
+            insertMealPlan(getMealID(dinner), dinner, "dinner");
+
+            System.out.println("Yeah! We planned the meals for " + weekday + ".\n");
+        }
+        printMealsForTheWeek();
+    }
+
+    public static void listBreakfastMeals() throws SQLException {
+        Connection conn = getConnection();
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM MEALS ORDER BY MEAL");
+        ResultSet rs = statement.executeQuery();
+        while (rs.next()) {
+            String category = rs.getString("category");
+            if (category.equals("breakfast")) {
+                String meal = rs.getString("meal");
+                System.out.println(meal);
+            }
+        }
+    }
+
+    public static void listLunchMeals() throws SQLException {
+        Connection conn = getConnection();
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM MEALS ORDER BY MEAL");
+        ResultSet rs = statement.executeQuery();
+        while (rs.next()) {
+            String category = rs.getString("category");
+            if (category.equals("lunch")) {
+                String meal = rs.getString("meal");
+                System.out.println(meal);
+            }
+        }
+    }
+
+    public static void listDinnerMeals() throws SQLException {
+        Connection conn = getConnection();
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM MEALS ORDER BY MEAL");
+        ResultSet rs = statement.executeQuery();
+        while (rs.next()) {
+            String category = rs.getString("category");
+            if (category.equals("dinner")) {
+                String meal = rs.getString("meal");
+                System.out.println(meal);
+            }
+        }
+    }
+
+    public static int getMealID(String userMealInput) throws SQLException {
+        Connection conn = getConnection();
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM MEALS");
+        ResultSet rs = statement.executeQuery();
+
+        while (rs.next()) {
+            int meal_id = rs.getInt("meal_id");
+            String mealName = rs.getString("meal");
+
+            if (mealName.equals(userMealInput)) {
+                return meal_id;
+            }
+        }
+        return -1;
+    }
+
+    public static void insertMealPlan(int meal_id, String mealName, String type) throws SQLException {
+        Connection conn = getConnection();
+
+        String sqlBreakfast = "INSERT INTO PLAN (meal_id, category, meal) VALUES (?, ?, ?)";
+        PreparedStatement statement = conn.prepareStatement(sqlBreakfast);
+
+        statement.setInt(1, meal_id);
+        statement.setString(2, type);
+        statement.setString(3, mealName);
+
+        statement.executeUpdate();
+        conn.close();
+    }
+
+    public static void printMealsForTheWeek() throws SQLException {
+        String[] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        Connection conn = getConnection();
+        PreparedStatement breakfastStatement = conn.prepareStatement("SELECT * FROM PLAN WHERE CATEGORY = 'breakfast'");
+        PreparedStatement lunchStatement = conn.prepareStatement("SELECT * FROM PLAN WHERE CATEGORY = 'lunch'");
+        PreparedStatement dinnerStatement = conn.prepareStatement("SELECT * FROM PLAN WHERE CATEGORY = 'dinner'");
+
+        ResultSet rsBreakfast = breakfastStatement.executeQuery();
+        ResultSet rsLunch = lunchStatement.executeQuery();
+        ResultSet rsDinner = dinnerStatement.executeQuery();
+
+        for (String day : weekdays) {
+            System.out.println(day);
+            rsBreakfast.next();
+            rsLunch.next();
+            rsDinner.next();
+            String breakfast = rsBreakfast.getString("meal");
+            String lunch = rsLunch.getString("meal");
+            String dinner = rsDinner.getString("meal");
+            System.out.println("Breakfast: " + breakfast);
+            System.out.println("Lunch: " + lunch);
+            System.out.println("Dinner: " + dinner + "\n");
         }
     }
 }
--- src/mealplanner/Main.java
+++ src/mealplanner/Main.java
@@ -6,13 +6,10 @@ import static mealplanner.Functions.*;
 
 public class Main {
   public static void main(String[] args) throws SQLException {
-    String DB_URL = "jdbc:postgresql:meals_db";
-    String USER = "postgres";
-    String PASS = "1111";
+    Connection connection = getConnection();
+    connection.setAutoCommit(true);
     int mealId = 1;
     int ingId = 1;
-    Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
-    connection.setAutoCommit(true);
     Statement statement = connection.createStatement();
     statement.executeUpdate("CREATE SEQUENCE IF NOT EXISTS sequence_meals");
     statement.executeUpdate("CREATE SEQUENCE IF NOT EXISTS sequence_ings");
@@ -27,17 +24,26 @@ public class Main {
             "ingredient_id INT PRIMARY KEY, " +
             "meal_id INT)");
 
+    statement.executeUpdate("CREATE TABLE IF NOT EXISTS plan " +
+            "(category VARCHAR, " +
+            "meal VARCHAR, " +
+            "meal_id INT)");
+
     Scanner scanner = new Scanner(System.in);
     List<List> items = new ArrayList<>();
     HashMap<String, String[]> ingList = new HashMap<>();
     while (true) {
-      System.out.println("What would you like to do (add, show, exit)?");
+      System.out.println("What would you like to do (add, show, plan, exit)?");
       String action = scanner.nextLine();
       if (action.toLowerCase().equals("add")) {
         add(items, ingList, mealId, ingId);
         mealId++;
+
       } else if (action.toLowerCase().equals("show")) {
         show(mealCategory());
+
+      } else if (action.toLowerCase().equals("plan")) {
+        plan();
 
       } else if (action.toLowerCase().equals("exit")) {
         System.out.println("Bye!");
